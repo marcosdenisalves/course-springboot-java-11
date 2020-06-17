a45937000bd5ac9f8e5f69174c097796dc8d079e
Marcos Dênis <dee.alves2@gmail.com>
2020-06-17 09:49:59 -0300

    User insert

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -11,16 +11,20 @@ import com.example.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
